1363fae8b399217fef5396957c766df267c93f96
Diego Basurco Reyes <d24reyes@gmail.com>
2021-01-26 19:57:50 -0500

    Fin CRUD Autor

--- Controllers/AutorController.cs
+++ Controllers/AutorController.cs
@@ -1,12 +1,14 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Models;
 
 namespace Controllers
 {
     [ApiController]
-    [Route("api")]
+    [Route("autor")]
     public class AutorController : ControllerBase
     {
         private readonly NoticiaDBContext _noticiaDbContext;
@@ -16,17 +18,92 @@ namespace Controllers
         }
 
         [HttpGet]
-        [Route("Autor")]
-        public List<Autor> ListaTodo(){
+        public List<Autor> ListaTodo()
+        {
             var autor = _noticiaDbContext.Autor.ToList();
             return autor;
         }
 
         [HttpGet]
-        [Route("Autor/{id}")]
-        public List<Autor> ListaAutor(int id){
+        [Route("{id}")]
+        public List<Autor> ListaAutor(int id)
+        {
             var autor = _noticiaDbContext.Autor.Where(y => y.AutorID == id).ToList();
             return autor;
+        }
+
+        [HttpPost]
+        public ActionResult Registrar(Autor a)
+        {
+            try
+            {
+                _noticiaDbContext.Autor.Add(a);
+                _noticiaDbContext.SaveChanges();
+                return Ok(new
+                {
+                    Status = 1,
+                    Message = "Autor registrado",
+                    Autor = a
+                });
+            }
+            catch (Exception ex)
+            {
+                return Ok(new
+                {
+                    Status = 0,
+                    Message = "No se pudo registrar",
+                    Detail = ex.Message
+                });
+            }
+        }
+
+        [HttpPut]
+        public ActionResult Update(Autor a)
+        {
+            try
+            {
+                _noticiaDbContext.Entry(a).State = EntityState.Modified;
+                _noticiaDbContext.SaveChanges();
+                return Ok(new
+                {
+                    Status = 1,
+                    Message = "Autor actualizado",
+                    Autor = a
+                });
+            }
+            catch (Exception ex)
+            {
+                return Ok(new
+                {
+                    Status = 0,
+                    Message = "No se pudo actualizar",
+                    Detail = ex.Message
+                });
+            }
+        }
+
+        [HttpDelete]
+        public ActionResult Delete(Autor a)
+        {
+            try
+            {
+                _noticiaDbContext.Autor.Remove(a);
+                _noticiaDbContext.SaveChanges();
+                return Ok(new
+                {
+                    Status = 1,
+                    Message = "Eliminado con Exito"
+                });
+            }
+            catch (Exception ex)
+            {
+                return Ok(new
+                {
+                    Status = 0,
+                    Message = "No se pudo eliminar",
+                    Detail = ex.Message
+                });
+            }
         }
     }
 }
